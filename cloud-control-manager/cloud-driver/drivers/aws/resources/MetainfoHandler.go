f85413b910e73728995ed95a03605f73a625f6d0
LMHbot <wsx1341@gmail.com>
2023-07-18 16:35:24 +0900

    GetAllRegionZone TEST 완료

--- cloud-control-manager/cloud-driver/drivers/aws/resources/MetainfoHandler.go
+++ cloud-control-manager/cloud-driver/drivers/aws/resources/MetainfoHandler.go
@@ -1,8 +1,6 @@
 package resources
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	call "github.com/cloud-barista/cb-spider/cloud-control-manager/cloud-driver/call-log"
@@ -14,33 +12,29 @@ type AwsMetainfoHandler struct {
 	Client         *ec2.EC2
 }
 
+
+// 모든 리전(권한이 없어도)을 조회하여 리스트로 가져와 AvailabilityZone을 리전당 조회합니다. 
 func (metaInfoHandler *AwsMetainfoHandler) GetAllRegionZone() ([]ec2.DescribeAvailabilityZonesOutput,error){
 	cblogger.Info("AWS Driver: called Metainfo()/GetAllRegionZone()!")
 
 	hiscallInfo := GetCallLogScheme(metaInfoHandler.Region, call.METAINFO, "Meta", "GetAllRegionZone()")
 	start := call.Start()
 
-	Regionsinput := &ec2.DescribeRegionsInput{
+	regionsInput := &ec2.DescribeRegionsInput{
 		AllRegions : aws.Bool(true),
 	}
-
-	
-
-
-	regionreq, regionresp := metaInfoHandler.Client.DescribeRegionsRequest(Regionsinput)
-	regionerr := regionreq.Send()
-	if regionerr != nil {
-		return nil, regionerr
+	regionReq, regionResp := metaInfoHandler.Client.DescribeRegionsRequest(regionsInput)
+	regionErr := regionReq.Send()
+	if regionErr != nil {
+		return nil, regionErr
 	}
-	fmt.Print(regionresp.Regions)
 
+	Zoneinput := &ec2.DescribeAvailabilityZonesInput{
+		AllAvailabilityZones : aws.Bool(true),
+	}
 	var availabilityZones []ec2.DescribeAvailabilityZonesOutput
-	for _, region := range regionresp.Regions {
-		Zoneinput := &ec2.DescribeAvailabilityZonesInput{
-			AllAvailabilityZones : aws.Bool(true),
-		}
+	for _, region := range regionResp.Regions {
 		metaInfoHandler.Client.Client.Config.Region = region.RegionName
-
 		zoneReq, zoneResp := metaInfoHandler.Client.DescribeAvailabilityZonesRequest(Zoneinput)
 		hiscallInfo.ElapsedTime = call.Elapsed(start)
 		zoneErr := zoneReq.Send()
@@ -50,10 +44,7 @@ func (metaInfoHandler *AwsMetainfoHandler) GetAllRegionZone() ([]ec2.DescribeAva
 		availabilityZones = append(availabilityZones, *zoneResp)
 	}
 
-
-
-
 	calllogger.Info(call.String(hiscallInfo))
-	//calllogger.Info(call.String(resp))
+
 	return availabilityZones, nil
 }
